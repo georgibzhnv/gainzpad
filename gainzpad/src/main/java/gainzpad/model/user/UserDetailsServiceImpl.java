ecfb5b71e4f2e945859f8b99b45ee535b0ba2906
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-05-15 21:49:07 +0300

    Added more templates

--- gainzpad/src/main/java/gainzpad/model/user/UserDetailsServiceImpl.java
+++ gainzpad/src/main/java/gainzpad/model/user/UserDetailsServiceImpl.java
@@ -1,31 +1,31 @@
-package gainzpad.model.user;
-
-import gainzpad.model.entity.UserEntity;
-import gainzpad.repository.UserRepository;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import java.util.Optional;
-
-public class UserDetailsServiceImpl implements UserDetailsService {
-
-    private final UserRepository userRepository;
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
-
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<UserEntity>userOpt = userRepository.findOneByEmail(username);
-
-        LOGGER.debug("Trying to load user{}. Successful? {}",
-                username,userOpt.isPresent());
-
-        return null;
-    }
-
-}
+//package gainzpad.model.user;
+//
+//import gainzpad.model.entity.UserEntity;
+//import gainzpad.repository.UserRepository;
+//import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import java.util.Optional;
+//
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//
+//    private final UserRepository userRepository;
+//    private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+//
+//    public UserDetailsServiceImpl(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        Optional<UserEntity>userOpt = userRepository.findOneByEmail(username);
+//
+//        LOGGER.debug("Trying to load user{}. Successful? {}",
+//                username,userOpt.isPresent());
+//
+//        return null;
+//    }
+//
+//}
--- gainzpad/src/main/java/gainzpad/web/WorkoutController.java
+++ gainzpad/src/main/java/gainzpad/web/WorkoutController.java
@@ -1,4 +1,9 @@
 package gainzpad.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/workout")
 public class WorkoutController {
 }
